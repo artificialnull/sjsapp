3591dc136f91c9752e409733d7ff3a44df34a095
Ishan Kamat <ishan@gabdeg.com>
2018-02-13 17:44:05 -0600

    developments with profile page and more
    added the bulk of the profile page, including fetching user info. still
    need to add getting and showing the users profile photo but that will be
    the next logical step forward.
    in addition, also changed some other layout info. i replaced a lot of
    hardcoded values in the xml files with string tags in strings.xml to be
    more compliant with the spec and to make changing things a little less
    painful. also changed some layout arrangement, padding, and text colors
    both to better fall in line with material design guidelines and to make
    overall app ui consistency a thing which exists to a greater degree
    (still some room to improve here but we can work on that as time
    progresses)

--- java/com/gabdeg/sjsapp/ProfileFragment.java
+++ java/com/gabdeg/sjsapp/ProfileFragment.java
@@ -1,17 +1,28 @@
 package com.gabdeg.sjsapp;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by ishan on 2/13/18.
  */
 
 public class ProfileFragment extends Fragment {
+
+    TextView profileFullName;
+    TextView profileEmail;
+    TextView profileStudentID;
+    TextView profileLockerNumber;
+    TextView profileLockerCombo;
+    ImageView profilePhoto;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,8 +34,45 @@ public class ProfileFragment extends Fragment {
 
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Profile");
 
+        profileFullName = (TextView) view.findViewById(R.id.profile_photo_title);
+        profileEmail = (TextView) view.findViewById(R.id.profile_photo_caption);
+        profileStudentID = (TextView) view.findViewById(R.id.profile_personal_id_info);
+        profileLockerNumber = (TextView) view.findViewById(R.id.profile_personal_locker_info);
+        profileLockerCombo = (TextView) view.findViewById(R.id.profile_personal_combo_info);
+        profilePhoto = (ImageView) view.findViewById(R.id.profile_photo_photo);
+
+        new GetUserMetadataTask().execute();
+
         return view;
 
+    }
+
+    private class GetUserMetadataTask extends AsyncTask<Void, Void, Browser.User> {
+        protected Browser.User doInBackground(Void... voids) {
+            Browser browser = new Browser();
+            browser.validateLogIn(
+                    PreferenceManager.getDefaultSharedPreferences(getActivity())
+                            .getString("username", "username"),
+                    PreferenceManager.getDefaultSharedPreferences(getActivity())
+                            .getString("password", "password")
+
+            );
+            return browser.getUserData();
+        }
+        protected void onPostExecute(Browser.User user) {
+            String fullName = user.getUserFirstName();
+            if (!user.getUserMiddleName().isEmpty()) {
+                fullName += " " + user.getUserMiddleName();
+            }
+            if (!user.getUserLastName().isEmpty()) {
+                fullName += " " + user.getUserLastName();
+            }
+            profileFullName.setText(fullName);
+            profileEmail.setText(user.getUserEmail());
+            profileStudentID.setText(user.getUserStudentID());
+            profileLockerNumber.setText(user.getUserLockerNumber());
+            profileLockerCombo.setText(user.getUserLockerCombo());
+        }
     }
 
 }
